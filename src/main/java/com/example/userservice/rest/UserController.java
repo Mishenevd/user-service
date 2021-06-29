071da0449e28fe0fa6667588cf868481e6b8d21b
Dmitriy Mishenyov <mishenev.8@gmail.com>
2021-06-29 11:53:52 +0300

    process user created

--- src/main/java/com/example/userservice/rest/UserController.java
+++ src/main/java/com/example/userservice/rest/UserController.java
@@ -1,12 +1,16 @@
 package com.example.userservice.rest;
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.model.ExternalResponse;
 import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
@@ -19,6 +23,11 @@ public class UserController {
     @PostMapping
     public Mono<Void> post(@RequestBody UserDto userDto) {
         return userService.createUser(userDto);
+    }
+
+    @GetMapping(value = "/process", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<ExternalResponse> process() {
+        return userService.processCreatedUsers();
     }
 
 }
